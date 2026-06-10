9c241ad1d408f8f9565e22518fa896830c8d36cb
Flipviduani <flipviduani@hotmail.com>
2026-06-10 12:56:59 -0300

    Product query development

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   imports: [
-    FormsModule
+    FormsModule, CommonModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
@@ -14,18 +15,22 @@ export class App {
 
   //Criando um objeto da biblioteca Http Client
   httpClient = inject(HttpClient);
+
+  //Variável Signal para armazenar a lista de produtos obtidos na API
+  produtos = signal<any[]>([]);
   
   //Atributo parar capturar o nome do produto
-  nomeProduto: string = ' ';
+  nomeProduto: string = '';
 
   //Função para ser executada quando o botão de pesquisa for clicado
   pesquisarProdutos(){
     
     //Fazendo uma requisição HTTP GET para consultar os produtos na API
     this.httpClient
-    .get('http://localhost:8081/api/v1/produtos/listar?nome=' + this.nomeProduto)
+    .get<any[]>('http://localhost:8081/api/v1/produtos/listar?nome=' + this.nomeProduto)
     .subscribe((data) => {//Capturando a resposta da API
-      console.log(data); //Exibindo a resposta no console do navegador
+      //Armazenar os dados obtidos da API no SIGNAL
+      this.produtos.set(data);
     });
   }
   
